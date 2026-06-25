b935d45b20d6b8be2e55c28012c302b858672120
nicemp <nicemp.contato@gmail.com>
2026-06-25 12:34:56 -0300

    Delete src directory

--- src/config/env.ts
+++
@@ -1,8 +0,0 @@
-export const env = {
-  siteName: import.meta.env.VITE_SITE_NAME ?? "Nicemp",
-  supabaseUrl: import.meta.env.VITE_SUPABASE_URL ?? "",
-  supabaseAnonKey: import.meta.env.VITE_SUPABASE_ANON_KEY ?? "",
-};
-
-export const isSupabaseConfigured =
-  env.supabaseUrl.length > 0 && env.supabaseAnonKey.length > 0;
